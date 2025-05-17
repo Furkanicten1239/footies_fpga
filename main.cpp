912428fcf6ae9e60d274e6eb87af65889e3e3bd4
Furkanicten1239 <88836416+Furkanicten1239@users.noreply.github.com>
2025-05-17 14:10:33 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@
 #include <verilated.h>
 #include "Vtop_game.h"
 
+// Display resolution
 const int H_RES = 640;
 const int V_RES = 480;
 
+// Pixel format for SDL texture (RGBA 32-bit)
 typedef struct Pixel {
     uint8_t a, b, g, r;
 } Pixel;
@@ -13,13 +15,17 @@ typedef struct Pixel {
 int main(int argc, char* argv[]) {
     Verilated::commandArgs(argc, argv);
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) return 1;
+    // ==== SDL INIT ====
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL init failed\n");
+        return 1;
+    }
 
+    // Allocate screen buffer and create SDL window
     Pixel screenbuffer[H_RES * V_RES] = {};
 
-    SDL_Window* window = SDL_CreateWindow("Pixel Controlled by Verilog",
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        H_RES, V_RES, SDL_WINDOW_SHOWN);
+    SDL_Window* window = SDL_CreateWindow("10x10 Blue Square",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, H_RES, V_RES, SDL_WINDOW_SHOWN);
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1,
         SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
@@ -27,7 +33,7 @@ int main(int argc, char* argv[]) {
     SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
         SDL_TEXTUREACCESS_STREAMING, H_RES, V_RES);
 
-    const Uint8* keyb_state = SDL_GetKeyboardState(NULL);
+    // ==== Instantiate and initialize Verilog module ====
     Vtop_game* top = new Vtop_game;
 
     top->sim_rst = 1;
@@ -36,38 +42,42 @@ int main(int argc, char* argv[]) {
     top->sim_rst = 0;
     top->clk_pix = 0; top->eval();
 
-    while (true) {
+    // ==== Main Loop ====
+    bool running = true;
+    while (running) {
+        // Clear inputs
         top->btn_up = 0;
-        top->btn_dn = keyb_state[SDL_SCANCODE_RIGHT];  // ← map Right Arrow to btn_dn
+        top->btn_dn = 0;
         top->btn_fire = 0;
 
-        // Tick the simulation
-        top->clk_pix = 1; top->eval();
-        top->clk_pix = 0; top->eval();
-
-        SDL_Event e;
-        while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) goto quit;
-        }
-        if (keyb_state[SDL_SCANCODE_Q]) break;
-
-        // Clear frame
+        // Clear screen buffer to black
         memset(screenbuffer, 0, sizeof(screenbuffer));
 
-        // Draw the pixel if enabled
-int x = top->sdl_sx;
-int y = top->sdl_sy;
-if (top->sdl_de && x < H_RES && y < V_RES) {
-    printf("DRAW at (%d, %d) Color = (%d, %d, %d)\n",
-           x, y, top->sdl_r, top->sdl_g, top->sdl_b);
+        // Run one full frame: 640 * 480 pixels
+        for (int i = 0; i < H_RES * V_RES; ++i) {
+            // Toggle clock
+            top->clk_pix = 1; top->eval();
+            top->clk_pix = 0; top->eval();
 
-    Pixel* p = &screenbuffer[y * H_RES + x];
-    p->a = 0xFF;
-    p->r = top->sdl_r;
-    p->g = top->sdl_g;
-    p->b = top->sdl_b;
-}
+            // Get pixel info from Verilog
+            int x = top->sdl_sx;
+            int y = top->sdl_sy;
 
+            // If sdl_de (draw enable) is high, draw the pixel
+            if (top->sdl_de && x < H_RES && y < V_RES) {
+                Pixel* p = &screenbuffer[y * H_RES + x];
+                p->a = 0xFF;
+                p->r = top->sdl_r;
+                p->g = top->sdl_g;
+                p->b = top->sdl_b;
+            }
+        }
+
+        // ==== SDL Render ====
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) running = false;
+        }
 
         SDL_UpdateTexture(texture, NULL, screenbuffer, H_RES * sizeof(Pixel));
         SDL_RenderClear(renderer);
@@ -75,7 +85,7 @@ if (top->sdl_de && x < H_RES && y < V_RES) {
         SDL_RenderPresent(renderer);
     }
 
-quit:
+    // ==== Cleanup ====
     top->final();
     SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
